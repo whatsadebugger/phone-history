0a3b40ee1ba8b6eb19062394d7c184bbe4c024b2
tabbakha, ahmad <tabba.ahmad@gmail.com>
2018-11-26 16:52:38 -0600

    make some basic changes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,6 +27,12 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+    <h3>Google</h3>
+    <ul>
+      <li><a href="https://google.com">Google</a></li>
+      <li><a href="https://google.com">Google</a></li>
+      <li><a href="https://google.com">Google</a></li>
+    </ul>
   </div>
 </template>
 
